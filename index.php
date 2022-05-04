2ec4be791a85573a2a75cb9a2beb3d5ee2c514b1
Terraprofissional <matheusst60@gmail.com>
2022-05-04 11:02:17 -0300

    serviço

---
+++ index.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+<meta charset="utf-8">
+<title>Matheus Silva Terra </title>
+<link rel="stylesheet" href="style.css">
+
+</head>
+
+<body>
+
+
+<header>
+<img src="naruto.jpg" alt="foto do naruto">
+<h1 id="title">Matheus Silva Terra</h1>
+</header>
+
+<section>
+<header> 
+    <h2 class="primeiro">Publicações</h2>
+
+</header>
+
+<article class="terceiro">
+    <header>
+<h3 class="segundo"> Imagem ilustrativa de HTML CODE </h3>
+
+<img src="1.jpg" alt="imagem ilustrativa" class="post_margem">
+
+<p class="post_coment"> 
+O <a href="https://rollingstone.uol.com.br/media/uploads/personagem_naruto_foto_reproducao.png" target= "_blank" class="link_naruto">Naruto</a> pode ser um pouco duro as vezes, talvez você não 
+saiba disso, mas o Naruto  também cresceu sem pai.
+Na verdade ele nunca conheceu nenhum de seus pais, e nunca
+teve nenhum amigo em nossa aldeia. 
+Mesmo assim eu nunca vi ele 
+chorar, ficar zangado ou se dar por vencido, 
+ele está sempre disposto a melhorar, ele quer ser respeitado,
+ é o sonho dele e o Naruto daria a vida por isso sem hesitar. 
+ Meu palpite é que ele se cansou de chorar e decidiu fazer 
+ alguma coisa a respeito! 
+ <a href="https://i.ytimg.com/vi/-uwyzs2XRh0/hqdefault.jpg" target="_blank" class="link_email_naruto">Naruto...?</a> 
+
+</p>
+
+</header>
+
+</article>
+
+</header>
+</section>
+
+<footer> 
+<ul class="lista">
+    <Li>
+   <a href="matheusst60@gmail.com" target="_blank" class="email">E-mail Matheus Terra</a>     
+
+</Li>
+<Li>
+    <a href="https://www.linkedin.com/in/matheus-silva-terra-907562171/" target="_blank" class="linkedin"> Linkedin</a>
+
+</Li>
+
+<Li>
+    <a href="https://www.youtube.com/watch?v=G3e-8EWdv8c" target="_blank" class="youtube"> Youtube </a>
+
+
+</ul>
+</footer>
+</body>
+
+</html>
+
